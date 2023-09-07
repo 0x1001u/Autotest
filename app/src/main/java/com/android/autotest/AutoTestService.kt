95f2ecb3185e5c5d110011d27f1be42ff1657071
0x1001u <x4u@qq.com>
2023-09-07 17:52:45 +0800

    first commit

--- app/src/main/java/com/android/autotest/AutoTestService.kt
+++ app/src/main/java/com/android/autotest/AutoTestService.kt
@@ -3,15 +3,61 @@ package com.android.autotest
 import android.accessibilityservice.AccessibilityService
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED
+import android.view.accessibility.AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED
+import android.view.accessibility.AccessibilityNodeInfo
+import java.lang.Thread.sleep
 
 class AutoTestService: AccessibilityService() {
     val TAG = javaClass.simpleName
+    companion object {
+//        const val SEARCH_VIEW_ID = "com.ss.android.ugc.aweme.splash.SplashActivity"
+        const val MAIN_VIEW_ID = "com.ss.android.ugc.aweme.main.MainActivity"
+        const val SEARCH_VIEW_ID = "com.ss.android.ugc.aweme:id/g7_"
+    }
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        Log.d(TAG, "onAccessibilityEvent: $event")
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+//        Log.d(TAG, "onAccessibilityEvent: $event")
+        if (event.eventType == TYPE_WINDOW_STATE_CHANGED ) {
+            when (event.className.toString()) {
+                MAIN_VIEW_ID -> {
+                    event.source?.let { source ->
+                        Log.d(TAG, "Search View ID: $source")
+                        source.getNodeById(SEARCH_VIEW_ID).click()
+                    }
+                }
+                else -> {
+                    Log.d(TAG, "Search Null: $event")
+                }
+            }
+        }
     }
 
     override fun onInterrupt() {
        Log.d(TAG, "onInterrupt")
+    }
+
+    fun AccessibilityNodeInfo?.click() {
+        if (this == null) return
+        if (this.isClickable) {
+            Log.d(TAG, "Clickable: $this")
+            this.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+            return
+        } else {
+            Log.d(TAG, "Don't Clickable: $this")
+            this.parent.click()
+        }
+    }
+
+    fun AccessibilityNodeInfo.getNodeById(id: String): AccessibilityNodeInfo? {
+        var count = 0
+        while (count < 10) {
+            findAccessibilityNodeInfosByViewId(id).let {
+                if (!it.isNullOrEmpty()) return it[0]
+            }
+            sleep(100)
+            count++
+        }
+        return null
     }
 }
--- app/src/main/java/com/android/autotest/MainActivity.kt
+++ app/src/main/java/com/android/autotest/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.android.autotest
 
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +15,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,12 +39,14 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MainView() {
         Column(
-            modifier = Modifier.padding(24.dp)
+            modifier = Modifier.padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text("Show the status of service")
 
             Button(onClick = {
-                startActivity()
+                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
 
             }) {
                Text("Enable Service")
@@ -52,7 +59,9 @@ class MainActivity : ComponentActivity() {
             }
 
             Button(onClick = {
-
+                startActivity(Intent(Intent.ACTION_VIEW).apply {
+                    component = ComponentName("com.ss.android.ugc.aweme", "com.ss.android.ugc.aweme.splash.SplashActivity")
+                })
             }) {
                 Text("Open Target")
             }
